e7d325685e92f220e69a0d6f8fdf020aac52da09
pateldeep1999 <122818168+pateldeep1999@users.noreply.github.com>
2023-01-16 22:43:48 +0530

    Add files via upload

---
+++ config.php
@@ -0,0 +1,10 @@
+<?php
+
+$server = "sql202.epizy.com";
+$username = "epiz_33231644";
+$password = "qvuJgooXQIZB3X";
+$dbName = "epiz_33231644_database";
+
+$con = mysqli_connect($server, $username, $password, $dbName) or die('Connection failed.');
+
+?>
